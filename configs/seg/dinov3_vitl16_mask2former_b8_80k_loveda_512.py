944b240d7c69dd67396a1ff8885374d9bc75a5b0
whu-pzhang <pzhang@whu.edu.cn>
2025-09-09 22:19:27 +0800

    add m2f config

---
+++ configs/seg/dinov3_vitl16_mask2former_b8_80k_loveda_512.py
@@ -0,0 +1,220 @@
+_base_ = [
+    "mmseg::_base_/default_runtime.py",
+    "mmseg::_base_/datasets/loveda.py",
+    "mmseg::_base_/schedules/schedule_80k.py",
+]
+
+custom_imports = dict(
+    imports=["src.dinov3_backbone", "mmdet.models"], allow_failed_imports=False
+)
+
+crop_size = (512, 512)
+data_preprocessor = dict(
+    type="SegDataPreProcessor",
+    size=crop_size,
+    mean=[123.675, 116.28, 103.53],
+    std=[58.395, 57.12, 57.375],
+    bgr_to_rgb=True,
+    pad_val=0,
+    seg_pad_val=255,
+)
+norm_cfg = dict(type="SyncBN", requires_grad=True)
+checkpoint_file = "weights/dinov3_vitl16_pretrain_lvd1689m-8aa4cbdd.pth"
+# checkpoint_file = "weights/dinov3_vitl16_pretrain_sat493m-eadcf0ff.pth"
+
+num_classes = 7
+model = dict(
+    type="EncoderDecoder",
+    data_preprocessor=data_preprocessor,
+    backbone=dict(
+        type="DINOv3Backbone",
+        repo_or_dir="dinov3",
+        model_name="dinov3_vitl16",
+        out_indices=(7, 11, 15, 23),
+        patch_size=16,
+        fp16=True,
+        frozen=True,
+        init_cfg=dict(type="Pretrained", checkpoint=checkpoint_file),
+    ),
+    neck=dict(
+        type="Feature2Pyramid",
+        embed_dim=1024,
+        rescales=[4, 2, 1, 0.5],
+        norm_cfg=norm_cfg,
+    ),
+    decode_head=dict(
+        type="Mask2FormerHead",
+        in_channels=[1024, 1024, 1024, 1024],
+        strides=[4, 8, 16, 32],
+        feat_channels=256,
+        out_channels=256,
+        num_classes=num_classes,
+        num_queries=100,
+        num_transformer_feat_level=3,
+        align_corners=False,
+        pixel_decoder=dict(
+            type="mmdet.MSDeformAttnPixelDecoder",
+            num_outs=3,
+            norm_cfg=dict(type="GN", num_groups=32),
+            act_cfg=dict(type="ReLU"),
+            encoder=dict(  # DeformableDetrTransformerEncoder
+                num_layers=6,
+                layer_cfg=dict(  # DeformableDetrTransformerEncoderLayer
+                    self_attn_cfg=dict(  # MultiScaleDeformableAttention
+                        embed_dims=256,
+                        num_heads=8,
+                        num_levels=3,
+                        num_points=4,
+                        im2col_step=64,
+                        dropout=0.0,
+                        batch_first=True,
+                        norm_cfg=None,
+                        init_cfg=None,
+                    ),
+                    ffn_cfg=dict(
+                        embed_dims=256,
+                        feedforward_channels=1024,
+                        num_fcs=2,
+                        ffn_drop=0.0,
+                        act_cfg=dict(type="ReLU", inplace=True),
+                    ),
+                ),
+                init_cfg=None,
+            ),
+            positional_encoding=dict(  # SinePositionalEncoding
+                num_feats=128, normalize=True
+            ),
+            init_cfg=None,
+        ),
+        enforce_decoder_input_project=False,
+        positional_encoding=dict(  # SinePositionalEncoding
+            num_feats=128, normalize=True
+        ),
+        transformer_decoder=dict(  # Mask2FormerTransformerDecoder
+            return_intermediate=True,
+            num_layers=9,
+            layer_cfg=dict(  # Mask2FormerTransformerDecoderLayer
+                self_attn_cfg=dict(  # MultiheadAttention
+                    embed_dims=256,
+                    num_heads=8,
+                    attn_drop=0.0,
+                    proj_drop=0.0,
+                    dropout_layer=None,
+                    batch_first=True,
+                ),
+                cross_attn_cfg=dict(  # MultiheadAttention
+                    embed_dims=256,
+                    num_heads=8,
+                    attn_drop=0.0,
+                    proj_drop=0.0,
+                    dropout_layer=None,
+                    batch_first=True,
+                ),
+                ffn_cfg=dict(
+                    embed_dims=256,
+                    feedforward_channels=2048,
+                    num_fcs=2,
+                    act_cfg=dict(type="ReLU", inplace=True),
+                    ffn_drop=0.0,
+                    dropout_layer=None,
+                    add_identity=True,
+                ),
+            ),
+            init_cfg=None,
+        ),
+        loss_cls=dict(
+            type="mmdet.CrossEntropyLoss",
+            use_sigmoid=False,
+            loss_weight=2.0,
+            reduction="mean",
+            class_weight=[1.0] * num_classes + [0.1],
+        ),
+        loss_mask=dict(
+            type="mmdet.CrossEntropyLoss",
+            use_sigmoid=True,
+            reduction="mean",
+            loss_weight=5.0,
+        ),
+        loss_dice=dict(
+            type="mmdet.DiceLoss",
+            use_sigmoid=True,
+            activate=True,
+            reduction="mean",
+            naive_dice=True,
+            eps=1.0,
+            loss_weight=5.0,
+        ),
+        train_cfg=dict(
+            num_points=12544,
+            oversample_ratio=3.0,
+            importance_sample_ratio=0.75,
+            assigner=dict(
+                type="mmdet.HungarianAssigner",
+                match_costs=[
+                    dict(type="mmdet.ClassificationCost", weight=2.0),
+                    dict(
+                        type="mmdet.CrossEntropyLossCost", weight=5.0, use_sigmoid=True
+                    ),
+                    dict(type="mmdet.DiceCost", weight=5.0, pred_act=True, eps=1.0),
+                ],
+            ),
+            sampler=dict(type="mmdet.MaskPseudoSampler"),
+        ),
+    ),
+    # model training and testing settings
+    train_cfg=dict(),
+    test_cfg=dict(mode="whole"),
+)
+
+# dataset config
+train_pipeline = [
+    dict(type="LoadImageFromFile"),
+    dict(type="LoadAnnotations", reduce_zero_label=True),
+    dict(
+        type="RandomChoiceResize",
+        scales=[int(512 * x * 0.1) for x in range(5, 21)],
+        resize_type="ResizeShortestEdge",
+        max_size=2048,
+    ),
+    dict(type="RandomCrop", crop_size=crop_size, cat_max_ratio=0.75),
+    dict(type="RandomFlip", prob=0.5),
+    dict(type="PhotoMetricDistortion"),
+    dict(type="PackSegInputs"),
+]
+train_dataloader = dict(batch_size=8, dataset=dict(pipeline=train_pipeline))
+
+
+# optimizer
+embed_multi = dict(lr_mult=1.0, decay_mult=0.0)
+optimizer = dict(
+    type="AdamW", lr=0.0001, weight_decay=0.05, eps=1e-8, betas=(0.9, 0.999)
+)
+optim_wrapper = dict(
+    _delete_=True,
+    type="OptimWrapper",
+    optimizer=optimizer,
+    clip_grad=dict(max_norm=0.01, norm_type=2),
+    paramwise_cfg=dict(
+        custom_keys={
+            "query_embed": embed_multi,
+            "query_feat": embed_multi,
+            "level_embed": embed_multi,
+        },
+        norm_decay_mult=0.0,
+    ),
+)
+
+
+param_scheduler = [
+    dict(type="LinearLR", start_factor=1e-6, by_epoch=False, begin=0, end=1500),
+    dict(
+        type="PolyLR",
+        eta_min=0.0,
+        power=1.0,
+        begin=1500,
+        end=80000,
+        by_epoch=False,
+    ),
+]
+
+default_hooks = dict(checkpoint=dict(interval=8000, max_keep_ckpts=1, save_best="mIoU"))
